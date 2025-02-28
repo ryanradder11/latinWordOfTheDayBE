58552434c3fb065f256e260d5736e2aa73e8ddfc
ryanradder <ryan.radder@headfirst.nl>
2025-02-28 22:38:12 +0100

    -basic crud working

---
+++ src/models/wordOfTheDay.model.ts
@@ -0,0 +1,16 @@
+export interface WordOfTheDay {
+    id: number;
+    word: string;
+    definition: string;
+    pronunciation: string;
+    origin: string;
+    example0: string;
+    example0Latin: string;
+    example1: string;
+    example1Latin: string;
+    example2: string;
+    example2Latin: string;
+    synonyms: string[];
+    antonyms: string[];
+    image: string;
+}
--- src/routes/addItem.js
+++ src/routes/addItem.js
@@ -1,13 +1,23 @@
 const db = require('../persistence');
-const { v4: uuid } = require('uuid');
 
 module.exports = async (req, res) => {
-    const item = {
-        id: uuid(),
-        name: req.body.name,
-        completed: false,
+    const wordOfTheDay = {
+        id: Math.floor(Math.random() * 1000000),
+        word: req.body.word,
+        definition: req.body.definition,
+        pronunciation: req.body.pronunciation,
+        origin: req.body.origin,
+        example0: req.body.example0,
+        example0Latin: req.body.example0Latin,
+        example1: req.body.example1,
+        example1Latin: req.body.example1Latin,
+        example2: req.body.example2,
+        example2Latin: req.body.example2Latin,
+        synonyms: req.body.synonyms,
+        antonyms: req.body.antonyms,
+        image: req.body.image,
     };
 
-    await db.storeItem(item);
-    res.send(item);
+    await db.storeItem(wordOfTheDay);
+    res.send(wordOfTheDay);
 };
